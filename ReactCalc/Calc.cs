d484fccc177dfe50200fd3b09689ab8e544ecc5f
kote0 <ytpyf.2@yandex.ru>
2017-06-26 20:27:05 +0500

    ДЗ 26.06.2017

--- ReactCalc/Calc.cs
+++ ReactCalc/Calc.cs
@@ -8,19 +8,53 @@ namespace ReactCalc
 {
     public class Calc
     {
+
         /// <summary>
         /// Калькулятор
         /// </summary>
-        /// <param name="x">Слагаемок</param>
-        /// <param name="y">Слагаемок</param>
-        /// <returns>Целое число</returns>
-        public int Sum(int x, int y)
+        /// <param name="action">Действие</param>
+        /// <param name="X">Первый параметр</param>
+        /// <param name="Y">Второй параметр</param>
+        /// <returns></returns>
+        public string Action(string action, double X, double Y)
         {
-            return x + y;
+            string res = "";
+            double resultNumber = 0;
+            switch (action)
+            {
+                case "/":
+                    if (Y == 0)
+                    {
+                        res = "Невозможно делить на 0!";
+                    } 
+                    else
+                    {
+                        resultNumber = X / Y;
+                        res = $"Результат деления = {resultNumber}";   
+                    }
+                    break;
+                case "*":
+                    resultNumber = X * Y;
+                    res = $"Результат умножения = {resultNumber}";
+                    break;
+                case "+":
+                    resultNumber = X + Y;
+                    res = $"Результат суммирования = {resultNumber}";
+                    break;
+                case "-":
+                    resultNumber = X - Y;
+                    res = $"Результат вычитания = {resultNumber}";
+                    break;
+                case "sqrt":
+                    resultNumber = Math.Sqrt(X);
+                    res = $"Результат получения квадратного корня = {resultNumber}";
+                    break;
+                default:
+                    res = "Неправильно введены данные";
+                    break;
+            }
+            return res;
         }
-        /*
-        public int X { get; set; }
-        public static int Y { get; set; }
-        */
+        
     }
 }
