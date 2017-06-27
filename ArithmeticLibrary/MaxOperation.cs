c192c5edd9afc5274bca7918821f2951ef10ee2e
kote0 <ytpyf.2@yandex.ru>
2017-06-28 01:43:18 +0500

    Добавлено MaxOperation

---
+++ ArithmeticLibrary/MaxOperation.cs
@@ -0,0 +1,27 @@
+﻿using CalcBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticLibrary
+{
+    class MaxOperation : IOperation
+    {
+        public long Code
+        {
+            get { return 1002; }
+        }
+
+        public string Name
+        {
+            get { return "max"; }
+        }
+
+        public double Execute(double[] args)
+        {
+            return args.Max();
+        }
+    }
+}
