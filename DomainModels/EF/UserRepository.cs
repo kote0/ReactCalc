f793ae080fdea2cddcd24ab1dc3a2a04612c5745
kote0 <ytpyf.2@yandex.ru>
2017-07-03 14:07:57 +0500

    EF

---
+++ DomainModels/EF/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using DomainModels.EF;
+using DomainModels.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModels.Models;
+
+namespace DomainModels.EF
+{
+    public class UserRepository : IUserRepository
+    {
+        private CalcContext context { get; set; }
+        public UserRepository()
+        {
+            this.context = new CalcContext();
+        }
+        public User Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string[]> Find(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(long Id)
+        {
+            
+            return context.Users
+                .FirstOrDefault(i => i.Id == Id);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return context.Users.ToList();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
